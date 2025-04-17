c38cbdef0ab3aa121c96db0d81ab67ea6efd7f1c
Second <drewuncle863724lu@gmail.com>
2025-04-17 11:54:08 +0800

    profile fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const PORT = 9000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
