666aa0d3f3735f2a7a41e119261648e8919df336
Second <drewuncle863724lu@gmail.com>
2025-04-21 11:13:45 +0800

    deploy

---
+++ buymeacoffee/app/setup/page.tsx
@@ -0,0 +1,10 @@
+const Setup = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-2xl font-bold">Setup Page</h1>
+        <p className="mt-4 text-gray-600">This is the setup page.</p>
+        </div>
+    );
+}
+
+export default Setup;
